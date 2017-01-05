58308c3eb77dacfc05823740f76c36555729e115
Matt Finucane <matfin@gmail.com>
2017-01-05 10:08:36 +0100

    Corrected points needed to render a square with two triangles.

--- OpenGL/One.cpp
+++ OpenGL/One.cpp
@@ -69,14 +69,27 @@ int one_main(void) {
      *  three point triangle.
      */
     float points_triangle_one[] = {
-        0.0f, 1.0f, 0.0f,   // starting at the top
-        0.5f, 0.0f, 0.0f,  // moving clockwise next
-        -0.5f, 0.0f, 0.0f  // finishing up triangle.
+        -0.5f, 1.0f, 0.0f,   // starting at the top
+        0.0f, 0.0f, 0.0f,  // moving clockwise next
+        -1.0f, 0.0f, 0.0f  // finishing up triangle.
     };
     float points_triangle_two[] = {
-        0.0f, 0.0f, 0.0f,   // starting at the top
-        0.5f, -1.0f, 0.0f,  // moving clockwise next
-        -0.5f, -1.0f, 0.0f  // finishing up triangle.
+        -0.5f, 0.0f, 0.0f,   // starting at the top
+        0.0f, -1.0f, 0.0f,  // moving clockwise next
+        -1.0f, -1.0f, 0.0f  // finishing up triangle.
+    };
+    /**
+     *  Creating a square with two triangles.
+     */
+    float points_square_one[] = {
+        1.0f, 1.0f, 0.0f,
+        1.0f, 0.0f, 0.0f,
+        0.0f, 0.0f, 0.0f
+    };
+    float points_square_two[] = {
+        0.0f, 1.0f, 0.0f,
+        1.0f, 1.0f, 0.0f,
+        0.0f, 0.0f, 0.0f
     };
     
     /**
@@ -114,6 +127,16 @@ int one_main(void) {
     glBindBuffer(GL_ARRAY_BUFFER, vbo_two);
     glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), points_triangle_two, GL_STATIC_DRAW);
     
+    GLuint vbo_square_one;
+    glGenBuffers(1, &vbo_square_one);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo_square_one);
+    glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), points_square_one, GL_STATIC_DRAW);
+    
+    GLuint vbo_square_two;
+    glGenBuffers(1, &vbo_square_two);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo_square_two);
+    glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), points_square_two, GL_STATIC_DRAW);
+    
     /**
      *  Next, we will use a VAO (vertex array object) which is used 
      *  to track references to one or more VBOs. It stores the memory
@@ -149,6 +172,20 @@ int one_main(void) {
     glBindVertexArray(vao_two);
     glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, vbo_two);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+    
+    GLuint vao_square_one;
+    glGenVertexArrays(1, &vao_square_one);
+    glBindVertexArray(vao_square_one);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo_square_one);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+    
+    GLuint vao_square_two;
+    glGenVertexArrays(1, &vao_square_two);
+    glBindVertexArray(vao_square_two);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo_square_two);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
     
     /**
@@ -221,6 +258,18 @@ int one_main(void) {
          *  Note: We need to call glDrawArrays twice!
          */
         glBindVertexArray(vao_two);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        
+        /**
+         *  Binding and drawing for triangle one for the square
+         */
+        glBindVertexArray(vao_square_one);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        
+        /**
+         *  Binding and drawing for triangle two for the square
+         */
+        glBindVertexArray(vao_square_two);
         glDrawArrays(GL_TRIANGLES, 0, 3);
         
         /**
